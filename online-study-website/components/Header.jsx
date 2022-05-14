f486ae754f0ece5e03287ef10140d9b3393c10f5
Khoi Nguyen <minhkhoi632000@gmail.com>
2022-05-14 12:13:41 -0700

    Use bootstrap to do the headers

--- online-study-website/components/Header.jsx
+++ online-study-website/components/Header.jsx
@@ -1,28 +1,41 @@
 import { navLinks } from '../src/data'
 import Link from 'next/link'
 import styles from './styles/Header.module.css'
+import {useRouter} from 'next/router'
 
 export default function Header () {
+    const cur_path = useRouter().pathname;
+
     return (
-        <header>
-            <Link href={'/'}>
-                <a className={styles.navName}>Online Study Prediction</a>
-            </Link>
-            <nav>
-                {
-                    navLinks.map((link, index) => {
-                        return (
-                            <ul className={styles.navList}>
-                                <Link href={link.path}>
-                                    {/* <li key={index} className={styles.navLink}> */}
-                                    <a>{link.name}</a>
-                                    {/* </li> */}
-                                </Link>
-                            </ul>
-                        )
-                    })
-                }
-            </nav>
-        </header>
+        <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+            <div className='container-fluid justify-content-between'>
+                <Link href='/'>
+                    <a className='navbar-brand mb-0 h1'>Online Study Prediction</a>
+                </Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link href='/'>
+                                <a className={cur_path == '/' ? 'nav-link active' : 'nav-link'} aria-current="page">Home</a>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link href='/about'>
+                                <a className={cur_path == '/about' ? 'nav-link active' : 'nav-link'}>About</a>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link href='/team'>
+                                <a className={cur_path == '/team' ? 'nav-link active' : 'nav-link'}>Team</a>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 }
