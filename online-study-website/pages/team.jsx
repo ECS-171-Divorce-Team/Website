4e465355de284229765f4276691f6646e47e9810
Khoi Nguyen <minhkhoi632000@gmail.com>
2022-05-24 22:15:27 -0700

    see if it works

--- online-study-website/pages/team.jsx
+++ online-study-website/pages/team.jsx
@@ -1,6 +1,39 @@
 import styles from '../styles/Team.module.css'
 import MemberDesc from "../components/MemberDesc";
-import { Members } from '../src/members';
+// import { Members } from '../src/members';
+
+const Members = [
+    {
+        name: "Justin Guan",
+        desc: "Hello, my name is Justin. I am currently a 3rd year in Computer Science. My hobbies include watching shows and traveling. One interesting fact is that I have watched all the Marvel Movies in theaters",
+        imgURL: '/teamPics/idino.png'
+    },
+    {
+        name: "Justin Guan",
+        desc: "Hello, my name is Justin. I am currently a 3rd year in Computer Science. My hobbies include watching shows and traveling. One interesting fact is that I have watched all the Marvel Movies in theaters",
+        imgURL: '/teamPics/idino.png'
+    },
+    {
+        name: "Justin Guan",
+        desc: "Hello, my name is Justin. I am currently a 3rd year in Computer Science. My hobbies include watching shows and traveling. One interesting fact is that I have watched all the Marvel Movies in theaters",
+        imgURL: '/teamPics/idino.png'
+    },
+    {
+        name: "Justin Guan",
+        desc: "Hello, my name is Justin. I am currently a 3rd year in Computer Science. My hobbies include watching shows and traveling. One interesting fact is that I have watched all the Marvel Movies in theaters",
+        imgURL: '/teamPics/idino.png'
+    },
+    {
+        name: "Justin Guan",
+        desc: "Hello, my name is Justin. I am currently a 3rd year in Computer Science. My hobbies include watching shows and traveling. One interesting fact is that I have watched all the Marvel Movies in theaters",
+        imgURL: '/teamPics/idino.png'
+    },
+    {
+        name: "Justin Guan",
+        desc: "Hello, my name is Justin. I am currently a 3rd year in Computer Science. My hobbies include watching shows and traveling. One interesting fact is that I have watched all the Marvel Movies in theaters",
+        imgURL: '/teamPics/idino.png'
+    },
+]
 
 export default function Team () {
     const rowData = []
