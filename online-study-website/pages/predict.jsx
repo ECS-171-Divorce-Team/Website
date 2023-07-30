f0375cfdc0293ff540f64898cdcfdd1ff105988c
Khoi Nguyen <koinguyen@ucdavis.edu>
2023-07-30 16:05:04 -0700

    testing cloud run

--- online-study-website/pages/predict.jsx
+++ online-study-website/pages/predict.jsx
@@ -49,7 +49,7 @@ export default function Predict () {
 
     const onSubmit = (event) => {
         event.preventDefault()
-        // fetch('http://127.0.0.1:5001/fir-function-demo-b59df/us-central1/prediction', {
+        // fetch('http://127.0.0.1:5001/online-learning-exp-pred/us-central1/prediction', {
         //     method: 'POST',
         //     headers: { 'Content-Type': 'application/json', },
         //     body: JSON.stringify(studentData)
@@ -58,7 +58,7 @@ export default function Predict () {
         //     console.log(`This is the result: ${obj}`)
         //     setResult(data.prediction)
         // })
-        fetch('https://prediction-rbp5lctcra-uc.a.run.app', {
+        fetch('https://hello-world-1-rbp5lctcra-uc.a.run.app/', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json', },
             body: JSON.stringify(studentData)
